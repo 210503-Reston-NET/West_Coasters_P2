d2243fcfe4cf6801a4b8e300c7c653a2186a83cf
Juniper Song <songminseonk@gmail.com>
2021-06-06 12:47:20 -0700

    scaffolded stuff so it builds

---
+++ HikePlanner/DL/EquipmentRepo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Models;
+
+namespace DL
+{
+    public class EquipmentRepo : IEquipmentRepo
+    {
+        public List<Equipment> GetAllEquipments()
+        {
+            return new List<Equipment>();
+        }
+
+        public Equipment AddEquipment(Equipment equipment)
+        {
+            return new Equipment();
+        }
+    }
+}
