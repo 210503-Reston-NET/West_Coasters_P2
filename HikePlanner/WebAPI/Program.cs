5ae917781a2c6fdefcf9196687c967b2f2e32418
Juniper Song <songminseonk@gmail.com>
2021-06-06 13:13:27 -0700

    Use Serilog for dev builds

--- HikePlanner/WebAPI/Program.cs
+++ HikePlanner/WebAPI/Program.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Serilog;
 
 namespace WebAPI
 {
@@ -13,7 +14,8 @@ namespace WebAPI
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            CreateHostBuilder(args).UseSerilog((hostingContext, loggerConfiguration) =>
+            loggerConfiguration.ReadFrom.Configuration(hostingContext.Configuration)).Build().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
