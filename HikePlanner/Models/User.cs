2d32847874a4446f0df018a2a5a79d25226d90ae
Juniper Song <songminseonk@gmail.com>
2021-06-06 14:24:06 -0700

    user Id discussion

--- HikePlanner/Models/User.cs
+++ HikePlanner/Models/User.cs
@@ -4,24 +4,24 @@ namespace Models
 {
     public class User
     {
-        public User()
-        {}
+        public User() {}
 
         public User(User user)
         {
             UserId = user.UserId;
             Name = user.Name;
             Email = user.Email;
+            Phone = user.Phone;
             Password = user.Password;
         }
 
-        //ToDo - confirm id field name : UUId vs UserId
-        public Guid UserId { get; set; }
+        //ToDo: Comeback after Auth0
+        //For now, use Guid.NewGuid().ToString() to get unique user Id
+        public string UserId { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
         public string Phone { get; set; }
         public int AddressId { get; set; }
-
     }
 }
