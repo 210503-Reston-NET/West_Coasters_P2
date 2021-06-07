6299543166a4c85a7bd0776bacc3f715e99cda29
Juniper Song <songminseonk@gmail.com>
2021-06-07 00:05:48 -0700

    added model unit tests for checklist and checklistitems

--- HikePlanner/Models/Checklist.cs
+++ HikePlanner/Models/Checklist.cs
@@ -1,21 +1,41 @@
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Models
 {
     public class Checklist
     {
+        private string _name;
+        private int _tripId;
         public Checklist()
         {
         }
 
         public int Id { get; set; }
-        public string Name { get; set; }
-
-        //ToDo: decide TripId or GroupId or UserId
-        // vote for TripId since this checklist can be visible to all group memebers (trip participants)
-        public int TripId { get; set; }
-
-        //public string UserId { get; set; }
+        public string Name
+        { 
+            get { return _name; }
+            set
+            {
+                if(value.Length == 0)
+                {
+                    throw new ValidationException("Checklist name cannot be empty");
+                }
+                _name = value;
+            }
+        }
+        public int TripId 
+        {
+            get { return _tripId; }
+            set
+            {
+                if(value <= 0)
+                {
+                    throw new ValidationException("Checklist TripId cannot be less than or equal to 0");
+                }
+                _tripId = value;
+            }
+        }
         private List<ChecklistItem> ChecklistItems { get; set; }
     }
 }
