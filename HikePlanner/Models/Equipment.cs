3402a3188d78ed7a6b653567955ef5f70ec1140c
Phoebe <jia.520@outlook.com>
2021-06-07 14:04:01 -0400

    add model tests for Equipment

--- HikePlanner/Models/Equipment.cs
+++ HikePlanner/Models/Equipment.cs
@@ -4,6 +4,20 @@ namespace Models
     {
         public Equipment()
         { }
+        public Equipment(string name)
+        { 
+            Name = name;
+        }
+
+        public Equipment(string name, string description) : this (name)
+        {
+            Description = description;
+        }
+
+        public Equipment(Equipment equipment) : this (equipment.Name, equipment.Description)
+        {
+            Id = equipment.Id;
+        }
 
         public int Id { get; set; }
         public string Name { get; set; }
