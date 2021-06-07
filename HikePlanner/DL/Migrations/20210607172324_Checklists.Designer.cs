2d2e8f7af812e89319fcc64235a9fbba57913799
Juniper Song <songminseonk@gmail.com>
2021-06-07 10:26:13 -0700

    checklist migration

--- HikePlanner/DL/Migrations/20210607172324_Checklists.Designer.cs
+++ HikePlanner/DL/Migrations/20210607172324_Checklists.Designer.cs
@@ -2,15 +2,17 @@
 using DL;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace DL.Migrations
 {
     [DbContext(typeof(AppDBContext))]
-    partial class AppDBContextModelSnapshot : ModelSnapshot
+    [Migration("20210607172324_Checklists")]
+    partial class Checklists
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
