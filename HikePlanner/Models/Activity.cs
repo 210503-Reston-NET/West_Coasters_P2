3da80f9f70b268cce8348a8bcfe916773e543006
Phoebe <jia.520@outlook.com>
2021-06-08 15:57:14 -0400

    updated Post and Activity Model

--- HikePlanner/Models/Activity.cs
+++ HikePlanner/Models/Activity.cs
@@ -9,14 +9,15 @@ namespace Models
         {
         }
 
-        public Activity(string name, string traidId, string trailhead)
+        public Activity(string name, string traidId, string trailhead, string creator)
         {
             Name = name;
             TrailId = traidId;
             TrailHead = trailhead;
+            Creator = creator;
         }
 
-        public Activity(int id, string name, string traidId, string trailhead) : this (name, traidId, trailhead)
+        public Activity(int id, string name, string traidId, string trailhead, string creator) : this (name, traidId, trailhead, creator)
         {
             Id = id;
         }
@@ -26,8 +27,7 @@ namespace Models
         public string Notes { get; set; }
         public string TrailId { get; set; }
         public string TrailHead { get; set; }
-
-        //public string Creator { get; set; }
+        public string Creator { get; set; }
 
         public List<Trip> Trips { get; set; }
     }
--- HikePlanner/Models/Post.cs
+++ HikePlanner/Models/Post.cs
@@ -11,10 +11,20 @@ namespace Models
             UserId = userId;
         }
 
+        public Post(string imageURL, int tripId, string userId, string Notes) : this(tripId, userId, Notes)
+        {
+            ImageURL = imageURL;
+        }
+
+        public Post(int id, int tripId, string userId, string Notes) : this(tripId, userId, Notes)
+        {
+            Id = id;
+        }
+
         public int Id { get; set; }
         public int TripId { get; set; }
         public string UserId { get; set; }
         public string Notes { get; set; }
-        //public string ImageURL { get; set; }
+        public string ImageURL { get; set; }
     }
 }
