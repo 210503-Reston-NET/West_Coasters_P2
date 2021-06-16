79d376c925db802f8f72e420973e00481e4e2dac
Phoebe <jia.520@outlook.com>
2021-06-16 09:24:09 -0700

    added bl test for trip and participant

---
+++ HikePlanner/Tests/ActivityBLTest.cs
@@ -0,0 +1,17 @@
+using Xunit;
+using System.Linq;
+using DL;
+using BL;
+using System.Collections.Generic;
+using Models;
+using System;
+using Moq;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    public class ActivityBLTest
+    {
+     
+    }
+}
