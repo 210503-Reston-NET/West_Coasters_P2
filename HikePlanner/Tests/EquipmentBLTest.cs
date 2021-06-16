233a710245f42b15e295a88b31dbae0589551ca6
Phoebe <jia.520@outlook.com>
2021-06-16 09:37:18 -0700

    added bl test for equipment

--- HikePlanner/Tests/EquipmentBLTest.cs
+++ HikePlanner/Tests/EquipmentBLTest.cs
@@ -12,6 +12,81 @@ namespace Tests
 {
     public class EquipmentBLTest
     {
-     
+        Mock<IEquipmentRepo> mockRepo;
+        public EquipmentBLTest()
+        {
+            mockRepo = new Mock<IEquipmentRepo>();
+        }
+        [Fact]
+        public async Task AddEquipmentAsync()
+        {
+            mockRepo.Setup(x => x.AddEquipmentAsync(It.IsAny<Equipment>())).Returns
+            (
+                Task.FromResult(
+                    new Equipment(){
+                        Id = 1
+                    }
+                )
+            );
+            
+            IEquipmentBL bl = new EquipmentBL(mockRepo.Object);
+            var test = await bl.AddEquipmentAsync(new Equipment());
+            int target = 1;
+            Assert.NotNull(test);
+            Assert.Equal(target, test.Id);
+        }
+        public async Task UpdateEquipmentAsync()
+        {
+            mockRepo.Setup(x => x.UpdateEquipmentAsync(It.IsAny<Equipment>())).Returns
+            (
+                Task.FromResult(
+                    new Equipment(){
+                        Id = 1
+                    }
+                )
+            );
+            
+            IEquipmentBL bl = new EquipmentBL(mockRepo.Object);
+            var test = await bl.UpdateEquipmentAsync(new Equipment());
+            int target = 1;
+            Assert.NotNull(test);
+            Assert.Equal(target, test.Id);
+        }
+
+        [Fact]
+        public async Task GetEquipmentByIdAsync()
+        {
+            mockRepo.Setup(x => x.GetEquipmentByIdAsync(It.IsAny<int>())).Returns
+            (
+                Task.FromResult(
+                    new Equipment(){
+                        Id = 1
+                    }
+                )
+            );
+            
+            IEquipmentBL bl = new EquipmentBL(mockRepo.Object);
+            var test = await bl.GetEquipmentByIdAsync(1);
+            int target = 1;
+            Assert.NotNull(test);
+            Assert.Equal(target, test.Id);
+        }
+
+        [Fact]
+        public async Task DeleteParticipantAsync()
+        {
+            mockRepo.Setup(x => x.DeleteEquipmentAsync(It.IsAny<Equipment>())).Returns
+            (
+                Task.FromResult(
+                    new Equipment(){
+                        Id = 1
+                    }
+                )
+            );
+            
+            IEquipmentBL bl = new EquipmentBL(mockRepo.Object);
+            await bl.DeleteEquipmentAsync(new Equipment());
+            Assert.NotNull(new Equipment());
+        }
     }
 }
